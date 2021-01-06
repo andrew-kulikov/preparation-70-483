67b504e6f9757d1d4c2f4bc6b6aeddda0a061d96
Andrew <andrew.1.kulikov@gmail.com>
2021-01-07 00:01:45 +0300

    Add project for module 1.4

---
+++ Preparation/EventsAndCallbacks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Common;
+
+namespace EventsAndCallbacks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var exampleSet = new ExampleSet()
+                ;
+
+            exampleSet.RunLast();
+
+            Console.ReadLine();
+        }
+    }
+}
